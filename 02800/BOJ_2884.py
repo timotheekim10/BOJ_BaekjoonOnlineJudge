cf52048711e1a03c1690cb021e7d41fbd5275957
timotheekim10 <timotheekim10@gmail.com>
2023-02-04 14:54:36 +0900

    알람 시계

---
+++ 02800/BOJ_2884.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+H, M = map(int, input().split())
+
+if M < 45:
+    if H == 0:
+        H = 23
+        M += 60
+    else:
+        H -= 1
+        M += 60
+
+print(H, M-45)
