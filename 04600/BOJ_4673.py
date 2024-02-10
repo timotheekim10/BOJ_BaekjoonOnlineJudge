160af3d92b8655bade7c90872fc38db5daa57dff
timotheekim10 <timotheekim10@gmail.com>
2024-02-10 17:09:43 +0900

    셀프 넘버

--- 04600/BOJ_4673.py
+++ 04600/BOJ_4673.py
@@ -1,19 +1,15 @@
 def notSelfNum(n):
-    if n < 10:
-        notSelfNums.append(n+n)
-    elif n < 100:
-        notSelfNums.append(n+n//10+n % 10)
-    elif n < 1000:
-        notSelfNums.append(n+n//100+n//10 % 10+n % 10)
-    elif n < 10000:
-        notSelfNums.append(n+n//1000+n//100 % 10+n//10 % 10+n % 10)
-
+    total = n
+    while n != 0:
+        total += n%10
+        n //= 10
+    return total
 
 nums = [num for num in range(1, 10001)]
 notSelfNums = []
 
 for i in range(1, 10000):
-    notSelfNum(i)
+    notSelfNums.append(notSelfNum(i))
 
 selfNums = sorted(list(set(nums)-set(notSelfNums)))
 for selfNum in selfNums:
