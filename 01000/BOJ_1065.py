dbd7c7b5c245e8ae1ad773e87c5677cc9a3d5e10
timotheekim10 <timotheekim10@gmail.com>
2024-02-11 21:06:12 +0900

    한수

--- 01000/BOJ_1065.py
+++ 01000/BOJ_1065.py
@@ -1,15 +1,15 @@
 cnt = 0
 
-N = int(input())
+n = int(input())
 
-for num in range(1, N+1):
+for num in range(1, n+1):
     if num < 10:
         cnt += 1
     elif num < 100:
         cnt += 1
     else:
-        strNum = str(num)
-        if int(strNum[0])-int(strNum[1]) == int(strNum[1])-int(strNum[2]):
+        str_num = str(num)
+        if int(str_num[0])-int(str_num[1]) == int(str_num[1])-int(str_num[2]):
             cnt += 1
 
 print(cnt)
