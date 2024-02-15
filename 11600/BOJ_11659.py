4bf17bc75a4853bae8c24a38130abaf962bcb737
timotheekim10 <timotheekim10@gmail.com>
2024-02-15 23:14:39 +0900

    구간 합 구하기 4

--- 11600/BOJ_11659.py
+++ 11600/BOJ_11659.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 sum = 0
-prefixSum = [0]
+dp = [0]
 
 n, m = map(int, input().split())
 nums = list(map(int, input().split()))
 
 for i in range(n):
-    sum += nums[i]
-    prefixSum.append(sum)
+    dp.append(dp[i] + nums[i])
 for k in range(m):
     i, j = map(int, input().split())
-    print(prefixSum[j] - prefixSum[i-1])
+    print(dp[j] - dp[i-1])
