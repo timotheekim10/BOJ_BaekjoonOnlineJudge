927d096818e49896500338a8eecdf0f99355ca30
timotheekim10 <timotheekim10@gmail.com>
2024-02-22 17:19:45 +0900

    구간 합 구하기 5

--- 11600/BOJ_11660.py
+++ 11600/BOJ_11660.py
@@ -1,16 +1,15 @@
 import sys
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 nums = [list(map(int, input().split())) for _ in range(n)]
-prefixSum = [[0]*(n+1) for _ in range(n+1)]
+dp = [[0]*(n+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        prefixSum[i][j] = prefixSum[i][j-1] + \
-            prefixSum[i-1][j] - prefixSum[i-1][j-1] + nums[i-1][j-1]
+        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + nums[i-1][j-1]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    print(prefixSum[x2][y2] - prefixSum[x2][y1-1] -
-          prefixSum[x1-1][y2] + prefixSum[x1-1][y1-1])
+    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1])
