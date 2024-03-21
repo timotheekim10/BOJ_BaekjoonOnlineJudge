c60137f0793c245e2ac5f4f1fe85d8c27a55e8ee
timotheekim10 <timotheekim10@gmail.com>
2024-03-21 22:34:47 +0900

    가장 긴 증가하는 부분 수열

---
+++ 11000/BOJ_11053.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+
+dp = [1]*n
+
+for i in range(1, n):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
