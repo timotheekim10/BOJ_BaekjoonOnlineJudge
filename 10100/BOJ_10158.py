cabbaa158596c0a4307d7e5112741f3d43af863e
timotheekim10 <timotheekim10@gmail.com>
2024-05-15 17:23:44 +0900

    개미

---
+++ 10100/BOJ_10158.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
+
+a = (p + t) // w
+b = (q + t) // h
+
+if a % 2 == 0:
+    x = (p + t) % w
+else:
+    x = w - (p + t) % w
+
+if b % 2 == 0:
+    y = (q + t) % h
+else:
+    y = h - (q + t) % h
+
+print(x, y)
