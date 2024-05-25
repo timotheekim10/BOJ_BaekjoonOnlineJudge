9fa74888b192188de0b6d7ffa0b421060446bcf4
timotheekim10 <timotheekim10@gmail.com>
2024-05-25 23:48:42 +0900

    집합의 표현

---
+++ 01700/BOJ_1717.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+sys.setrecursionlimit(1000000)
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+def find_parent(x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent[x])
+    return parent[x]
+
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+for _ in range(m):
+    command, a, b = map(int, input().split())
+    if command == 0:
+        union_parent(a, b)
+    elif command == 1:
+        if find_parent(a) == find_parent(b):
+            print('YES')
+        else:
+            print('NO')
